e99f4a4a467deb49e80bc2ed7265d75e5978e421
Yoely@peneh <yoely@peneh.com>
2023-11-27 23:27:09 -0500

    email

--- app/Http/Controllers/mailController.php
+++ app/Http/Controllers/mailController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\contactForm;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class mailController extends Controller
 {
@@ -27,50 +30,58 @@ class mailController extends Controller
             return response()->json(['success' => false, 'msg' => 'Message Required']);
         endif;
 
+        $mailData = [
+            'title' => 'Mail from StoneWorxCorp.com',
+            'name' => $form['name'],
+            'email' => $form['email'],
+            'subject' => $form['subject'],
+            'message' => $form['message']
+        ];
+         
+        try{
 
-        #send Email
-        $to = "office@stoneworxcorp.com, jacob@stoneworxcorp.com";
-        $subject = $form['subject'];
+            Mail::to('info@peneh.com')->send(new contactForm($mailData));
 
-        $message = "
-            <html>
-            <head>   <title>{$form['subject']}</title>    </head>
+            return response()->json(['success' => true, 'msg' => 'Thank you for getting in touch!<br>
+            We appreciate you contacting us about <b>' . $form['subject'] . '</b>.<br> One of our customer happiness members will be getting back to you shortly. <br> Have a great day!']);
+        
+        } catch(Exception){
 
-            <body> 
+            return response()->json(['success' => false, 'msg' => 'Error Sending. Try Again Later']);
 
-            <p>{$form['message']}</p>
-
-            <small>This request was submitted thru your website</small>
-
-            </body>
-            </html>
-        ";
-
-// Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// More headers
-$headers .= "From: {$form['name']} <{$form['email']}>" . "\r\n";
-//$headers .= 'Cc: myboss@example.com' . "\r\n";
-
-$mail_sent = mail($to,$subject,$message,$headers);
+        }
 
 
-#end Email
+//         #send Email
+//         $to = "office@stoneworxcorp.com, jacob@stoneworxcorp.com";
+//         $subject = $form['subject'];
 
-if($mail_sent):
-       
-    return response()->json(['success' => true, 'msg' => 'Thank you for getting in touch!<br>
-    We appreciate you contacting us about <b>' . $form['subject'] . '</b>.<br> One of our customer happiness members will be getting back to you shortly. <br> Have a great day!']);
+//         $message = "
+//             <html>
+//             <head>   <title>{$form['subject']}</title>    </head>
+
+//             <body> 
+
+//             <p>{$form['message']}</p>
+
+//             <small>This request was submitted thru your website</small>
+
+//             </body>
+//             </html>
+//         ";
+
+// // Always set content-type when sending HTML email
+// $headers = "MIME-Version: 1.0" . "\r\n";
+// $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// // More headers
+// $headers .= "From: {$form['name']} <{$form['email']}>" . "\r\n";
+// //$headers .= 'Cc: myboss@example.com' . "\r\n";
+
+// $mail_sent = mail($to,$subject,$message,$headers);
 
 
-else:
-    return response()->json(['success' => false, 'msg' => 'Error Sending. Try Again Later']);
-
-
-endif;
-
-
+// #end Email
     }
+
 }
